25244f3c431fe36800086a34d4bc8ba45408bf0b
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-14 22:12:04 +0530

    add the book Now button on navbar

--- components/reusableComp/Navbar.tsx
+++ components/reusableComp/Navbar.tsx
@@ -1,15 +1,17 @@
 'use client'
+import { MoveRight } from "lucide-react";
 import { links } from "@/utils/data";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Navbar: React.FC = () => {
 
 
   return (
     <nav className="p-4">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* */}
+      <div className="w-full flex justify-between items-center">
+        {/* logo */}
         <div className="flex items-center">
           <Link href="/">
             <Image
@@ -17,17 +19,20 @@ const Navbar: React.FC = () => {
               alt="logo"
               width={128}
               height={48}
-              className="cursor-pointer w-32 h-12"
+              className="cursor-pointer w-40 h-12"
             />
           </Link>
         </div>
-        <div className="flex space-x-4">
+        {/* links */}
+        <div className="flex space-x-10">
           {links.map((link, index) => (
             <Link key={index} href={link.href}>
-              <span className="text-lg">{link.label}</span>
+              <span className="text-xl">{link.label}</span>
             </Link>
           ))}
         </div>
+        {/* book the session */}
+        <button className="px-10  flex flex-row gap-2 border border-black rounded-full py-3 items-center group me-20"  ><span>Book Now</span>  <MoveRight className="group-hover:translate-x-2 transition" /></button>
       </div>
     </nav>
   );
