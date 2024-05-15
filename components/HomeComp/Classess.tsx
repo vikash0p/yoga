ed697308362cc0d040f54d01e1614a4613e5bfed
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-15 21:22:19 +0530

    complete the classess components

--- components/HomeComp/Classess.tsx
+++ components/HomeComp/Classess.tsx
@@ -3,10 +3,12 @@ import MyButton from '../reusableComp/MyButton';
 
 const Classess = () => {
   return (
-    <div>
-      <div className="">
-        <h1 className='tex-3xl md:text-4xl lg:text-5xl xl:text-6xl'>Choose Your Class</h1>
-        <MyButton text='View All' />
+    <div className=" py-24">
+      <div className=" w-full flex flex-row justify-between px-1 md:px-4 lg:px-10 items-center ">
+        <h1 className="tex-4xl md:text-4xl lg:text-5xl xl:text-6xl font-serif">
+          Choose Your Class
+        </h1>
+        <MyButton text="View All" />
       </div>
       <div className=""></div>
     </div>
