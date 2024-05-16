33c34c1f3380b62ac551a379ad69ae3873d6566d
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-16 21:07:45 +0530

    add the not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,46 @@
+"use client";
+import MyButton from "@/components/reusableComp/MyButton";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+    const{theme}=useTheme()
+  return (
+    <div className="w-full h-screen flex flex-col justify-center py-20 items-center">
+      <div className="w-full h-full m-auto ">
+        <div className="relative max-w-sm md:max-w-2xl lg:max-w-4xl h-full m-auto">
+          {/* {theme === "light" && (
+            <Image
+              src={"/light.png"}
+              alt="not-found"
+              fill
+              className="object-contain object-center"
+              sizes="100vw"
+            />
+          )} */}
+          {theme === "dark" ? (
+            <Image
+              src={"/404.png"}
+              alt="not-found"
+              fill
+              className="object-contain object-center"
+              sizes="100vw"
+            />
+          ) : (
+            <Image
+              src={"/light.png"}
+              alt="not-found"
+              fill
+              className="object-contain object-center"
+              sizes="100vw"
+            />
+          )}
+        </div>
+      </div>
+      <Link href="/">
+        <MyButton text="Return Home" />{" "}
+      </Link>
+    </div>
+  );
+}
