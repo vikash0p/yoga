7d202e550271dc401d029c69834cd7d193d59e8c
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-16 11:36:48 +0530

    complete the queries components

--- components/HomeComp/AccordionDemo.tsx
+++ components/HomeComp/AccordionDemo.tsx
@@ -5,28 +5,136 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { useTheme } from "next-themes";
 
 export function AccordionDemo() {
+    const{theme}=useTheme();
   return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <Accordion type="single" collapsible className="w-full space-y-6">
+      {/* first */}
+      <AccordionItem
+        value="item-1"
+        className={`bg-[#d4cbc2] p-4 ${theme === "dark" && "bg-gray-800"}`}
+      >
+        <AccordionTrigger className="md:text-2xl">
+          Best & essential yoga accessories for beginners?
+        </AccordionTrigger>
         <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
+          <div>
+            <p className="text-lg">
+              Enim lobortis scelerisque fermentum dui. Morbi enim nunc faucibus
+              a pellentesque sit amet. Aliquet nibh praesent tristique magna sit
+              amet purus gravida.Ridiculus mus mauris vitae ultricies leo
+              integer malesuada nunc vel. At tellus at urna condimentum mattis.
+            </p>
+            <p className="text-lg">
+              Dictumst quisque sagittis purus sit amet volutpat consequat.
+              Viverra maecenas accumsan lacus vel facilisis volutpat est velit
+              egestas. Gravida arcu ac tortor dignissim convallis. Aliquam etiam
+              erat gravida arcu ac tortor velit scelerisque in dictum non.
+            </p>
+          </div>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
+
+      {/* second */}
+      <AccordionItem
+        value="item-2"
+        className={`bg-[#d4cbc2] p-4 ${theme === "dark" && "bg-gray-800"}`}
+      >
+        <AccordionTrigger className="md:text-2xl">
+          Best & essential yoga accessories for beginners?
+        </AccordionTrigger>
         <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+          <div>
+            <p className="text-lg">
+              Enim lobortis scelerisque fermentum dui. Morbi enim nunc faucibus
+              a pellentesque sit amet. Aliquet nibh praesent tristique magna sit
+              amet purus gravida.Ridiculus mus mauris vitae ultricies leo
+              integer malesuada nunc vel. At tellus at urna condimentum mattis.
+            </p>
+            <p className="text-lg">
+              Dictumst quisque sagittis purus sit amet volutpat consequat.
+              Viverra maecenas accumsan lacus vel facilisis volutpat est velit
+              egestas. Gravida arcu ac tortor dignissim convallis. Aliquam etiam
+              erat gravida arcu ac tortor velit scelerisque in dictum non.
+            </p>
+          </div>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
+      {/* third */}
+      <AccordionItem
+        value="item-3"
+        className={`bg-[#d4cbc2] p-4 ${theme === "dark" && "bg-gray-800"}`}
+      >
+        <AccordionTrigger className="md:text-2xl">
+          Best & essential yoga accessories for beginners?
+        </AccordionTrigger>
         <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
+          <div>
+            <p className="text-lg">
+              Enim lobortis scelerisque fermentum dui. Morbi enim nunc faucibus
+              a pellentesque sit amet. Aliquet nibh praesent tristique magna sit
+              amet purus gravida.Ridiculus mus mauris vitae ultricies leo
+              integer malesuada nunc vel. At tellus at urna condimentum mattis.
+            </p>
+            <p className="text-lg">
+              Dictumst quisque sagittis purus sit amet volutpat consequat.
+              Viverra maecenas accumsan lacus vel facilisis volutpat est velit
+              egestas. Gravida arcu ac tortor dignissim convallis. Aliquam etiam
+              erat gravida arcu ac tortor velit scelerisque in dictum non.
+            </p>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      {/* fourth */}
+      <AccordionItem
+        value="item-4"
+        className={`bg-[#d4cbc2] p-4 ${theme === "dark" && "bg-gray-800"}`}
+      >
+        <AccordionTrigger className="md:text-2xl">
+          Best & essential yoga accessories for beginners?
+        </AccordionTrigger>
+        <AccordionContent>
+          <div>
+            <p className="text-lg">
+              Enim lobortis scelerisque fermentum dui. Morbi enim nunc faucibus
+              a pellentesque sit amet. Aliquet nibh praesent tristique magna sit
+              amet purus gravida.Ridiculus mus mauris vitae ultricies leo
+              integer malesuada nunc vel. At tellus at urna condimentum mattis.
+            </p>
+            <p className="text-lg">
+              Dictumst quisque sagittis purus sit amet volutpat consequat.
+              Viverra maecenas accumsan lacus vel facilisis volutpat est velit
+              egestas. Gravida arcu ac tortor dignissim convallis. Aliquam etiam
+              erat gravida arcu ac tortor velit scelerisque in dictum non.
+            </p>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      {/* five */}
+      <AccordionItem
+        value="item-5"
+        className={`bg-[#d4cbc2] p-4 ${theme === "dark" && "bg-gray-800"}`}
+      >
+        <AccordionTrigger className="md:text-2xl">
+          Best & essential yoga accessories for beginners?
+        </AccordionTrigger>
+        <AccordionContent>
+          <div>
+            <p className="text-lg">
+              Enim lobortis scelerisque fermentum dui. Morbi enim nunc faucibus
+              a pellentesque sit amet. Aliquet nibh praesent tristique magna sit
+              amet purus gravida.Ridiculus mus mauris vitae ultricies leo
+              integer malesuada nunc vel. At tellus at urna condimentum mattis.
+            </p>
+            <p className="text-lg">
+              Dictumst quisque sagittis purus sit amet volutpat consequat.
+              Viverra maecenas accumsan lacus vel facilisis volutpat est velit
+              egestas. Gravida arcu ac tortor dignissim convallis. Aliquam etiam
+              erat gravida arcu ac tortor velit scelerisque in dictum non.
+            </p>
+          </div>
         </AccordionContent>
       </AccordionItem>
     </Accordion>
--- components/HomeComp/Queries.tsx
+++ components/HomeComp/Queries.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import { AccordionDemo } from './AccordionDemo';
 
 const Queries = () => {
   return (
@@ -8,10 +9,13 @@ const Queries = () => {
       className="w-full min-h-screen py-10 mt-10"
       style={{ backgroundImage: "url('/Group.png')" }}
     >
-      <div className="max-w-5xl h-full mx-auto border border-yellow-300">
+      <div className="max-w-5xl h-full mx-auto ">
         <div className='text-center space-y-5 lg:space-y-10 px-1 md:px-5 lg:px-0 '>
           <h2 className='text-3xl md:text-4xl lg:text-5xl font-serif font-semibold  '>Commonly Posing Queries </h2>
           <p className='text-lg md:w-3/4 lg:w-1/2 m-auto'>Purus non enim praesent elementum facilisis leo. Mattis ullamcorper velit sed ullamcorper morbi. Sed felis eget velit /aliquet. Volutpat blandit erat.</p>
+        </div>
+        <div className='py-10'>
+            <AccordionDemo />
         </div>
       </div>
     </div>
