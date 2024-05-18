edbc81359aa34e5955c2a0d306ecd2c558688840
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-18 17:10:58 +0530

    add the metadata login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,22 @@
 import LoginForm from '@/components/FormComponents/LoginForm'
 import ReusableHero from '@/components/reusableComp/ReusableHero'
 import React from 'react'
+import { Metadata } from "next";
+
+export const loginMetadata: Metadata = {
+  title: "Login - Mantra",
+  description:
+    "Access your Mantra account to manage your classes, profile, and more. Log in to continue your yoga journey.",
+  applicationName: "Mantra",
+  category: "fitness",
+  openGraph: {
+    title: "Login - Mantra",
+    description:
+      "Access your Mantra account to manage your classes, profile, and more. Log in to continue your yoga journey.",
+    url: "https://mantra.example.com/login",
+    type: "website",
+  },
+};
 
 const Login = () => {
   return (
