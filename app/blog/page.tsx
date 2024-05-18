57c4c0988b667800c3e182578e5ca238c0b3af00
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-18 17:08:47 +0530

    add the metadata for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,22 @@
 import BlogData from '@/components/BlogComponent/BlogData'
 import ReusableHero from '@/components/reusableComp/ReusableHero'
 import React from 'react'
+import { Metadata } from "next";
+
+export const blogMetadata: Metadata = {
+  title: "Blog - Mantra",
+  description:
+    "Read our latest blog posts on yoga, wellness, and holistic living. Stay informed and inspired with Mantra.",
+  applicationName: "Mantra",
+  category: "fitness",
+  openGraph: {
+    title: "Blog - Mantra",
+    description:
+      "Read our latest blog posts on yoga, wellness, and holistic living. Stay informed and inspired with Mantra.",
+    url: "https://mantra.example.com/blog",
+    type: "website",
+  },
+};
 
 const Blog = () => {
   return (
