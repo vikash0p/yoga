4e89fbc78372ba2a4783db64bea9d440dc74e09f
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-18 17:11:39 +0530

    add the metadata register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,22 @@
 import RegisterForm from "@/components/FormComponents/RegisterForm";
 import ReusableHero from "@/components/reusableComp/ReusableHero";
 import React from "react";
+import { Metadata } from "next";
+
+export const registerMetadata: Metadata = {
+  title: "Register - Mantra",
+  description:
+    "Join the Mantra community by creating an account. Start your holistic yoga journey with us today.",
+  applicationName: "Mantra",
+  category: "fitness",
+  openGraph: {
+    title: "Register - Mantra",
+    description:
+      "Join the Mantra community by creating an account. Start your holistic yoga journey with us today.",
+    url: "https://mantra.example.com/register",
+    type: "website",
+  },
+};
 
 const Register = () => {
   return (
