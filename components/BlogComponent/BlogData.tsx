32150d4e1c220c208bfbaec35903339304769252
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-18 12:22:08 +0530

    complete the blog page

--- components/BlogComponent/BlogData.tsx
+++ components/BlogComponent/BlogData.tsx
@@ -1,11 +1,36 @@
-'use client'
-import { blog } from '@/utils/data';
-import { elsie } from '@/utils/font';
-import Image from 'next/image';
-import React from 'react'
-import MyButton from '../reusableComp/MyButton';
+"use client";
+import { useState, useEffect } from "react";
+import { blog } from "@/utils/data";
+import { elsie } from "@/utils/font";
+import Image from "next/image";
+import React from "react";
+import MyButton from "../reusableComp/MyButton";
+import BlogPagination from "./BlogPagination";
 
-const BlogData = () => {
+interface Blog {
+  blogId: string;
+  blogImg: string;
+  blogTitle: string;
+}
+
+const BlogData: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
+  const [currentBlogs, setCurrentBlogs] = useState<Blog[]>([]);
+
+  useEffect(() => {
+    // Fetch the paginated blogs (simulated here with slicing the blog array)
+    const fetchBlogs = () => {
+      const startIndex = (currentPage - 1) * postsPerPage;
+      const endIndex = startIndex + postsPerPage;
+      setCurrentBlogs(blog.slice(startIndex, endIndex));
+    };
+
+    fetchBlogs();
+  }, [currentPage, postsPerPage]);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <div className="w-full py-5 md:py-7 lg:py-10">
       <div className="text-center w-1/2 m-auto ">
@@ -19,27 +44,33 @@ const BlogData = () => {
         </p>
       </div>
       <div className="w-full md:px-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 py-20">
-        {blog.map((blog) => {
-          return (
-            <div key={blog.blogId} className="space-y-5">
-              <div className="relative w-full h-80">
-                <Image
-                  src={blog.blogImg}
-                  alt={blog.blogTitle}
-                  fill
-                  className="object-cover object-center rounded-2xl"
-                />
-              </div>
-              <h5 className={`${elsie.className} line-clamp-2 text-xl md:text-2xl`}>
-                {blog.blogTitle}{" "}
-              </h5>
-              <MyButton text='Read More' />
+        {currentBlogs.map((blog) => (
+          <div key={blog.blogId} className="space-y-5">
+            <div className="relative w-full h-80">
+              <Image
+                src={blog.blogImg}
+                alt={blog.blogTitle}
+                fill
+                className="object-cover object-center rounded-2xl"
+              />
             </div>
-          );
-        })}
+            <h5
+              className={`${elsie.className} line-clamp-2 text-xl md:text-2xl`}
+            >
+              {blog.blogTitle}
+            </h5>
+            <MyButton text="Read More" />
+          </div>
+        ))}
       </div>
+      <BlogPagination
+        postsPerPage={postsPerPage}
+        totalPosts={blog.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </div>
   );
-}
+};
 
-export default BlogData
+export default BlogData;
--- components/BlogComponent/BlogPagination.tsx
+++ components/BlogComponent/BlogPagination.tsx
@@ -1,38 +1,50 @@
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
+"use client";
+import React from "react";
 
-export function BlogPagination() {
-  return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#" isActive>
-            2
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
-  );
+interface BlogPaginationProps {
+  postsPerPage: number;
+  totalPosts: number;
+  paginate: (pageNumber: number) => void;
+  currentPage: number;
 }
+
+const BlogPagination: React.FC<BlogPaginationProps> = ({
+  postsPerPage,
+  totalPosts,
+  paginate,
+  currentPage,
+}) => {
+
+
+  const pageNumbers: number[] = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const handleClick = (number: number) => {
+    paginate(number);
+    window.scrollTo({ top: 500, behavior: "smooth" });
+  };
+
+  return (
+    <nav>
+      <ul className="flex justify-center space-x-2">
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={`cursor-pointer px-3 border py-1 rounded hover:bg-gray-900 hover:text-white hover:transition-colors hover:duration-500 hover:ease-linear hover:border-white ${
+              number === currentPage
+                ? "bg-gray-800 text-white"
+                : "bg-gray-200 text-black"
+            }`}
+          >
+            <a onClick={() => handleClick(number)}>{number}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default BlogPagination;
