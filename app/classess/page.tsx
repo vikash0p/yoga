93476564db30a0e747d24356518305c1bae31b1f
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-18 17:08:07 +0530

    add the metadata for clesses page

--- app/classess/page.tsx
+++ app/classess/page.tsx
@@ -4,6 +4,22 @@ import Review from "@/components/ClassesComp/Review";
 import Yoga from "@/components/ClassesComp/Yoga";
 import ReusableHero from "@/components/reusableComp/ReusableHero";
 import React from "react";
+import { Metadata } from "next";
+
+export const classesMetadata: Metadata = {
+  title: "Classes - Mantra",
+  description:
+    "Explore our diverse range of yoga classes designed for all levels. Find the perfect class to enhance your practice.",
+  applicationName: "Mantra",
+  category: "fitness",
+  openGraph: {
+    title: "Classes - Mantra",
+    description:
+      "Explore our diverse range of yoga classes designed for all levels. Find the perfect class to enhance your practice.",
+    url: "https://mantra.example.com/classes",
+    type: "website",
+  },
+};
 
 const Class = () => {
   return (
