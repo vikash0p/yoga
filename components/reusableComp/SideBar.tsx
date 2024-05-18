4f3896a9713bd331eaa629ae9ce90b53be2a9d13
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-18 16:05:29 +0530

    sidebar add the data

--- components/reusableComp/SideBar.tsx
+++ components/reusableComp/SideBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 
 import {
@@ -10,27 +11,48 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { links } from "@/utils/data";
 import { Menu } from "lucide-react";
-
+import Image from "next/image";
+import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
 export function Sidebar() {
   return (
-    <Sheet >
+    <Sheet>
       <SheetTrigger asChild>
-        <Button variant="link"><Menu/></Button>
+        <Button variant="link">
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when youre done.
-          </SheetDescription>
+          <SheetTitle>
+            <div className="flex items-center">
+              <Link href="/" className="relative w-40 h-12">
+                <Image
+                  src="/logo.svg"
+                  alt="logo"
+                  fill
+                  priority
+                  sizes="(max-width: 768px) 100vw, 200px"
+                  className="cursor-pointer w-40 h-12 object-contain aspect-auto"
+                />
+              </Link>
+            </div>
+          </SheetTitle>
         </SheetHeader>
-
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
+        <div>
+          <div className="flex flex-col gap-5 mt-10">
+            {links.map((link, index) => (
+              <Link key={index} href={link.href}>
+                <span className="text-xl">{link.label}</span>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className="mt-8">
+          <ModeToggle />
+        </div>
       </SheetContent>
     </Sheet>
   );
